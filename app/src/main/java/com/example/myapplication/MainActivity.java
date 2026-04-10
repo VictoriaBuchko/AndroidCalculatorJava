5a290cbfe521fdb5126b4be7d6e9814f1aa6f943
ViktoriiaBuchko <buckovika0@gmail.com>
2026-04-10 17:40:40 +0300

    Initial Android Calculator project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView resultField;
+    EditText numberField;
+    TextView operationField;
+
+    Double operand = null;
+    String lastOperation = "=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultField    = findViewById(R.id.resultField);
+        numberField    = findViewById(R.id.numberField);
+        operationField = findViewById(R.id.operationField);
+
+        findViewById(R.id.clear).setOnClickListener(view -> onClearClick());
+
+        findViewById(R.id.add).setOnClickListener(view -> onOperationClick("+"));
+        findViewById(R.id.sub).setOnClickListener(view -> onOperationClick("-"));
+        findViewById(R.id.mul).setOnClickListener(view -> onOperationClick("*"));
+        findViewById(R.id.div).setOnClickListener(view -> onOperationClick("/"));
+        findViewById(R.id.eq).setOnClickListener(view  -> onOperationClick("="));
+
+        findViewById(R.id.n0).setOnClickListener(view -> onNumberClick("0"));
+        findViewById(R.id.n1).setOnClickListener(view -> onNumberClick("1"));
+        findViewById(R.id.n2).setOnClickListener(view -> onNumberClick("2"));
+        findViewById(R.id.n3).setOnClickListener(view -> onNumberClick("3"));
+        findViewById(R.id.n4).setOnClickListener(view -> onNumberClick("4"));
+        findViewById(R.id.n5).setOnClickListener(view -> onNumberClick("5"));
+        findViewById(R.id.n6).setOnClickListener(view -> onNumberClick("6"));
+        findViewById(R.id.n7).setOnClickListener(view -> onNumberClick("7"));
+        findViewById(R.id.n8).setOnClickListener(view -> onNumberClick("8"));
+        findViewById(R.id.n9).setOnClickListener(view -> onNumberClick("9"));
+        findViewById(R.id.comma).setOnClickListener(view -> onNumberClick(","));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if (operand != null) {
+            outState.putDouble("OPERAND", operand);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        lastOperation = savedInstanceState.getString("OPERATION");
+        operand = savedInstanceState.getDouble("OPERAND");
+
+        if (operand != null) {
+            resultField.setText(operand.toString().replace('.', ','));
+        }
+
+        operationField.setText(lastOperation);
+    }
+
+    public void onNumberClick(String number) {
+        numberField.append(number);
+
+        if (lastOperation.equals("=") && operand != null) {
+            operand = null;
+        }
+    }
+
+    public void onOperationClick(String op) {
+        String number = numberField.getText().toString();
+
+        if (!number.isEmpty()) {
+            number = number.replace(',', '.');
+
+            try {
+                performOperation(Double.valueOf(number), op);
+            } catch (NumberFormatException ex) {
+                numberField.setText("");
+            }
+        }
+
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+
+    private void performOperation(Double number, String operation) {
+        if (operand == null) {
+            operand = number;
+        } else {
+
+            if (lastOperation.equals("=")) {
+                lastOperation = operation;
+            }
+
+            switch (lastOperation) {
+                case "=":
+                    operand = number;
+                    break;
+                case "/":
+                    operand = (number == 0) ? 0.0 : operand / number;
+                    break;
+                case "*":
+                    operand *= number;
+                    break;
+                case "+":
+                    operand += number;
+                    break;
+                case "-":
+                    operand -= number;
+                    break;
+            }
+        }
+
+        resultField.setText(operand.toString().replace('.', ','));
+        numberField.setText("");
+    }
+
+    public void onClearClick() {
+        numberField.setText("");
+        resultField.setText("0");
+        operationField.setText("=");
+
+        operand = null;
+        lastOperation = "=";
+    }
+}
